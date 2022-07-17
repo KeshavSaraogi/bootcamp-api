435080227f08105f8448302865b3d83dac5d9fb3
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2022-07-17 17:13:07 +0530

    setup
    setting up env with express

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const dotenv = require("dotenv")
+
+dotenv.config({ path: "./config/config.env" })
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.listen(
+  PORT,
+  console.log(`Server Running In ${process.env.NODE_ENV} Mode on PORT ${PORT}`)
+)
