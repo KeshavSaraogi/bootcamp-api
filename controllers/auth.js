8f259fc3b2f8cbf098cd6edf6292bdd9e0941376
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2022-07-22 17:06:44 +0530

    forgot Password functionality
    -- forgotPassword
    -- send Email to request email
    -- get Password Reset token
    -- reset Password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const User = require("../models/User")
 const ErrorResponse = require("../utilities/errorResponse")
 const asyncHandler = require("../middleware/async")
+const sendEmail = require("../utilities/sendEmail")
+const crypto = require("crypto")
 
 // description  -- Register Users
 // route        -- POST /api/auth/register
@@ -71,9 +73,78 @@ const getMe = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, data: user })
 })
 
+// description  -- Forgot Password
+// route        -- GET /api/auth/forgotPassword
+// access       -- Private
+const forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email })
+  if (!user) {
+    return next(new ErrorResponse("No User With This Email"), 404)
+  }
+  // Get Reset token
+  const resetToken = user.getResetPasswordToken()
+
+  await user.save({ validateBeforeSave: false })
+
+  const resetURL = `${
+    req.protocol
+  }://${req.getHost()}/api/auth/resetpassword/${resetToken}`
+
+  const message = `You are recieving this email because you have requested the reset of a password. 
+  Please make a PUT request to: \n\n ${resetURL}`
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Password Reset Token",
+      message: message,
+    })
+    res.status(200).json({ Success: true, data: "Email Sent" })
+  } catch (err) {
+    console.log(err)
+    user.resetPasswordToken = undefined
+    user.resetPasswordExpire = undefined
+
+    await user.save({ validateBeforeSave: false })
+    return next(new ErrorResponse("Email Could Not Be Sent"), 500)
+  }
+  res.status(200).json({ success: true, data: user })
+})
+
+// description  -- Reset Password
+// route        -- PUT /api/auth/resetpassword/:resettoken
+// access       -- Public
+
+const resetPassword = asyncHandler(async (req, res, next) => {
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.resettoken)
+    .digest("hex")
+
+  const user = await User.findByOne({
+    resetPasswordToken: resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  })
+
+  if (!user) {
+    return next(new ErrorResponse("Invalid Token"), 400)
+  }
+
+  //Set new password
+  user.password = req.body.password
+
+  user.resetPasswordToken = undefined
+  user.resetPasswordExpire = undefined
+
+  await user.save()
+  sendTokenResponse(user, 200, res)
+})
+
 module.exports = {
   register: register,
   login: login,
   sendTokenResponse: sendTokenResponse,
   getMe: getMe,
+  forgotPassword: forgotPassword,
+  resetPassword: resetPassword,
 }
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
+const crypto = require("crypto")
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -41,6 +42,10 @@ const UserSchema = new mongoose.Schema({
 })
 
 UserSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    next()
+  }
+
   const salt = await bcrypt.genSalt(10)
   this.password = await bcrypt.hash(this.password, salt)
 })
@@ -55,6 +60,20 @@ UserSchema.methods.getSignedJWTToken = function () {
 // Match User Entered Password To The Hashed password
 UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
+}
+
+//Generate, Hash Password Token
+UserSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString("hex")
+
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex")
+
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000
+
+  return resetToken
 }
 
 module.exports = mongoose.model("User", UserSchema)
